1bd2a0678e648e306b92551f357c82f5e97f6fd6
owner <owner@Owner01>
2018-07-03 14:56:18 -0400

    Creating parallel coefficient arrays to use summation with limits to determine accuracy.
    Switch to parsing via EarthXyzToCoefs.java

--- VsopLibMain/src/vsoplibmain/EarthtXyzToCoefs.java
+++ VsopLibMain/src/vsoplibmain/EarthtXyzToCoefs.java
@@ -25,6 +25,53 @@ public class EarthtXyzToCoefs {
    public static final String XYZPATHNAM = "D:\\JOrbit\\VSOP\\VsopLib\\src\\vsop\\EarthXyzFuncs.java";
    public static final String COFPATHNAM = "D:\\JOrbit\\VSOP\\VsopLibMain\\src\\vsoplibmain\\EarthCoefs.java";
 
+   public static enum Abc {
+      A, B, C
+   }
+
+   public static enum Xyz {
+      X, Y, Z
+   }
+
+   public static class FuncNumKey {
+
+      public Xyz function;
+      public int number;
+
+      public FuncNumKey(Xyz function, int number) {
+         this.function = function;
+         this.number = number;
+      }
+
+      @Override
+      public int hashCode() {
+         int hash = 5;
+         return hash;
+      }
+
+      @Override
+      public boolean equals(Object obj) {
+         if (this == obj) {
+            return true;
+         }
+         if (obj == null) {
+            return false;
+         }
+         if (getClass() != obj.getClass()) {
+            return false;
+         }
+         final FuncNumKey other = (FuncNumKey) obj;
+         if (this.number != other.number) {
+            return false;
+         }
+         if (this.function != other.function) {
+            return false;
+         }
+         return true;
+      }
+
+   }
+
    public static void main(String[] args) {
 
       try {
@@ -37,7 +84,7 @@ public class EarthtXyzToCoefs {
                  + " * To change this template file, choose Tools | Templates\n"
                  + " * and open the template in the editor.\n"
                  + " */\n"
-                 + "package vsop;\n"
+                 + "package vsoplibmain;\n"
                  + "\n"
                  + "/**\n"
                  + " *\n"
@@ -55,28 +102,38 @@ public class EarthtXyzToCoefs {
                  + "};\n";
          coefsFW.write(output);
 
-         Map<String, Map<String, List<Double>>> arrayNameToValues = new HashMap();
+         Map<FuncNumKey, Map<Abc, List<Double>>> funcNumAbcDoubles = new HashMap();
+         String function = null;
+         String number = null;
+         FuncNumKey funcNumKey = null;
+         List<Double> aDoubles = null;
+         List<Double> bDoubles = null;
+         List<Double> cDoubles = null;
          FileReader xyzFR = new FileReader(XYZPATHNAM);
          BufferedReader br = new BufferedReader(xyzFR);
          String line;
          while ((line = br.readLine()) != null) {
             if (line.contains("double")) {
                if (!line.contains("//")) {
-                  System.out.println("\n");
-                  String arrayName = null;
-                  List<Double> aValues = new ArrayList();
-                  List<Double> bValues = new ArrayList();
-                  List<Double> cValues = new ArrayList();
+//                  System.out.println("\n");
+
+                  aDoubles = new ArrayList();
+                  bDoubles = new ArrayList();
+                  cDoubles = new ArrayList();
+
                   while (((line = br.readLine()) != null) && (!line.contains("return"))) {
 
                      String[] tokens = line.trim().split(" ");
 //                     for (int i = 0; i < tokens.length; i++) {
 //                        System.out.println("tokens[" + i + "] = " + tokens[i]);
 //                     }
-                     arrayName = tokens[0];
+                     function = tokens[0].substring(0, 1);
+//                     System.out.println("function = " + function);
+                     number = tokens[0].substring(1);
+//                     System.out.println("number = " + number);
                      String aValue = tokens[2].replace(";", "");
-//                    System.out.println("aValue = " + aValue);
-                     aValues.add(Double.parseDouble(aValue));
+//                    System.out.println("aValue = " + aValue);                   
+                     aDoubles.add(Double.parseDouble(aValue));
                      String bValue = "0";
                      String cValue = "0";
                      if (line.contains("cos")) {
@@ -85,31 +142,80 @@ public class EarthtXyzToCoefs {
                         cValue = tokens[6];
 //                        System.out.println("cValue = " + cValue);
                      }
-                     bValues.add(Double.parseDouble(bValue));
-                     cValues.add(Double.parseDouble(cValue));
+                     bDoubles.add(Double.parseDouble(bValue));
+                     cDoubles.add(Double.parseDouble(cValue));
 
                   }
-//                  System.out.println("arrayName = " + arrayName);
-//                  for (int i = 0; i < aValues.size(); i++) {
-//                     System.out.println("aValues.get(" + i + ") = " + aValues.get(i));
-                  Map<String, List<Double>> abcValues = new HashMap();
-                  abcValues.put("A", aValues);
-                  abcValues.put("B", bValues);
-                  abcValues.put("C", cValues);
-                  arrayNameToValues.put(arrayName, abcValues);
+
+                  Map<Abc, List<Double>> abcDoubles = new HashMap();
+                  abcDoubles.put(Abc.A, aDoubles);
+                  abcDoubles.put(Abc.B, bDoubles);
+                  abcDoubles.put(Abc.C, cDoubles);
+
+                  Xyz funct = null;
+                  if (function.contains("X")) {
+                     funct = Xyz.X;
+                  } else if (function.contains("Y")) {
+                     funct = Xyz.Y;
+                  } else if (function.contains("Z")) {
+                     funct = Xyz.Z;
+                  }
+
+                  funcNumKey = new FuncNumKey(funct, Integer.parseInt(number));
+                  funcNumAbcDoubles.put(funcNumKey, abcDoubles);
+                  Map<Abc, List<Double>> abcListDouble = funcNumAbcDoubles.getOrDefault(funcNumKey, new HashMap());
+                  //                 System.out.println("System.out.printlnabcListDouble = " + abcListDouble);
                }
             }
          }
 
-         Map<String, List<Double>> abcValues;
-         Coefficients.SixGroups sixGroups[] = Coefficients.SixGroups.values();
-         for (Coefficients.SixGroups sixGroup : sixGroups) {
-            abcValues = arrayNameToValues.get(sixGroup.toString());
-            System.out.println("sixGroup = " + sixGroup);
-            System.out.println ("abcValues = " + abcValues);
+         Abc abcs[] = Abc.values();
+         for (Abc abc : abcs) {
+//            System.out.println("abc.toString = " + abc.toString());
+            Xyz xyzs[] = Xyz.values();
+            for (Xyz xyz : xyzs) {
+//               System.out.println("xyz.toString() = " + xyz.toString());
+               output = "public static final double[][] "
+                       + abc.toString() + xyz.toString()
+                       + " = {\n{";
+               for (int i = 0; i < Coefficients.NUMGROUPS; i++) {
+//                  System.out.println("output = " + output);
+                  funcNumKey = new FuncNumKey(xyz, i);
+//                 System.out.println("funcNumKey = " + funcNumKey);
+                  Map<Abc, List<Double>> abcListDouble = funcNumAbcDoubles.getOrDefault(funcNumKey, null);
+                  List<Double> listDoubles = abcListDouble.getOrDefault(abc, null);
+                  Double[] doubleArray = new Double[listDoubles.size()];
+                  doubleArray = (Double[]) listDoubles.toArray(doubleArray);
+
+                  for (int j = 0; j < doubleArray.length; j++) {
+                     output += doubleArray[j];
+                     //System.out.println("output = " + output);
+                     if (j < (doubleArray.length - 1)) {
+                        output += ",\n";
+                     } else {
+                        output += "\n";
+                     }
+                  }
+                  if (i < (Coefficients.NUMGROUPS - 1)) {
+                     output += "},\n{";
+                  } else {
+                     output += "}\n";
+                  }
+               }
+
+               output += "};\n";
+               //                 System.out.println("output = " + output);
+               coefsFW.write(output);
+               coefsFW.flush();
+
+            }
+            //           System.out.println("output = " + output);
+            //coefsFW.write(output);
+            //coefsFW.flush();
          }
 
          output = "}\n";
+         System.out.println("output = " + output);
          coefsFW.write(output);
 
          coefsFW.flush();
@@ -118,7 +224,8 @@ public class EarthtXyzToCoefs {
          xyzFR.close();
 
       } catch (Exception ex) {
-         Logger.getLogger(EarthtXyzToCoefs.class.getName()).log(Level.SEVERE, null, ex);
+         Logger.getLogger(EarthtXyzToCoefs.class
+                 .getName()).log(Level.SEVERE, null, ex);
       }
    }
 
