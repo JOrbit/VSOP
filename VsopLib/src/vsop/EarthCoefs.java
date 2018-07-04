5ffa57f9176f97c975771f2fc49dc2825660949f
owner <owner@Owner01>
2018-07-04 13:18:11 -0400

    Creating parallel coefficient arrays to use summation with limits to determine accuracy.
    Switch to parsing via EarthXyzToCoefs.java
    
    In progress.

--- VsopLib/src/vsop/EarthCoefs.java
+++ VsopLib/src/vsop/EarthCoefs.java
@@ -17,13 +17,66 @@ import java.util.logging.Logger;
  * @author owner
  */
 public class EarthCoefs {
+
    public static final String COEFPATHNAM = "D:\\JOrbit\\VSOP\\VsopLib\\src\\vsop\\EarthCoefs.txt";
+
+   public static final int[] XTERMS = {843, 491, 204, 18, 15, 6};
+   public static final int[] YTERMS = {854, 496, 202, 17, 15, 6};
+   public static final int[] ZTERMS = {178, 120, 53, 12, 6, 2};
 
    private List<Double> coefs = null;
 
    public EarthCoefs() {
       this.coefs = new ArrayList();
       this.parseDataFile();
+   }
+
+   public double[][] createAX() {
+      double AX[][] = new double[Coefs.NUMGROUPS][];
+
+      for (int i = 0; i < Coefs.NUMGROUPS; i++) {
+         AX[i] = new double[XTERMS[i]];
+      }
+
+      int fromIndex = 0;
+      int toIndex = 0;
+      for (int i = 0; i < Coefs.NUMGROUPS; i++) {
+         toIndex = EarthCoefs.XTERMS[i] + fromIndex;
+         List<Double> adList = this.coefs.subList(fromIndex, toIndex);
+         int j = 0;
+         for (Double ad : adList) {
+            AX[i][j++] = ad;
+         }
+         fromIndex = toIndex;
+      }
+
+      return AX;
+   }
+
+   public double[][] createAY() {
+      double AY[][] = new double[Coefs.NUMGROUPS][];
+
+      for (int i = 0; i < Coefs.NUMGROUPS; i++) {
+         AY[i] = new double[YTERMS[i]];
+      }
+
+      int fromIndex = 0;
+      for (int i = 0; i < Coefs.NUMGROUPS; i++) {
+         fromIndex += XTERMS[i];
+      }
+
+      int toIndex = 0;
+      for (int i = 0; i < Coefs.NUMGROUPS; i++) {
+         toIndex = EarthCoefs.YTERMS[i] + fromIndex;
+         List<Double> adList = this.coefs.subList(fromIndex, toIndex);
+         int j = 0;
+         for (Double ad : adList) {
+            AY[i][j++] = ad;
+         }
+         fromIndex = toIndex;
+      }
+
+      return AY;
    }
 
    private void parseDataFile() {
@@ -44,6 +97,5 @@ public class EarthCoefs {
    public List<Double> getCoefs() {
       return coefs;
    }
-   
-   
+
 }
--- VsopLib/test/vsop/EarthCoefsTest.java
+++ VsopLib/test/vsop/EarthCoefsTest.java
@@ -5,6 +5,7 @@
  */
 package vsop;
 
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,22 +18,22 @@ import static org.junit.Assert.*;
  * @author owner
  */
 public class EarthCoefsTest {
-   
+
    public EarthCoefsTest() {
    }
-   
+
    @BeforeClass
    public static void setUpClass() {
    }
-   
+
    @AfterClass
    public static void tearDownClass() {
    }
-   
+
    @Before
    public void setUp() {
    }
-   
+
    @After
    public void tearDown() {
    }
@@ -41,6 +42,55 @@ public class EarthCoefsTest {
    public void testSomeMethod() {
       EarthCoefs earthCoefs = new EarthCoefs();
       System.out.println("earthCoefs.getCoefs().size() = " + earthCoefs.getCoefs().size());
+      int terms = 0;
+      for (int i = 0; i < Coefs.NUMGROUPS; i++) {
+         terms += 3 * (EarthCoefs.XTERMS[i] + EarthCoefs.YTERMS[i] + EarthCoefs.ZTERMS[i]);
+      }
+      System.out.println("terms = " + terms);
    }
-   
+
+   /**
+    * Test of createAX method, of class EarthCoefs.
+    */
+   @Test
+   public void testCreateAX() {
+      System.out.println("createAX");
+      EarthCoefs instance = new EarthCoefs();
+      double[][] result = instance.createAX();
+      for (int i = 0; i < Coefs.NUMGROUPS; i++) {
+         for (int j = 0; j < EarthCoefs.XTERMS[i]; j++) {
+            System.out.println("AX[" + i + "][" + j + "] = " + result[i][j]);
+         }
+      }
+   }
+
+   /**
+    * Test of getCoefs method, of class EarthCoefs.
+    */
+   @Test
+   public void testGetCoefs() {
+      System.out.println("getCoefs");
+      EarthCoefs instance = new EarthCoefs();
+      List<Double> expResult = null;
+      //     List<Double> result = instance.getCoefs();
+      //   assertEquals(expResult, result);
+      // TODO review the generated test code and remove the default call to fail.
+   }
+
+   /**
+    * Test of createAY method, of class EarthCoefs.
+    */
+   @Test
+   public void testCreateAY() {
+      System.out.println("createAY");
+      EarthCoefs instance = new EarthCoefs();
+      double[][] result = instance.createAY();
+      for (int i = 0; i < Coefs.NUMGROUPS; i++) {
+         for (int j = 0; j < EarthCoefs.YTERMS[i]; j++) {
+            System.out.println("AY[" + i + "][" + j + "] = " + result[i][j]);
+         }
+      }
+
+   }
+
 }
