82a35a8813da30e960f96a243feb635f1b321f2b
owner <owner@Owner01>
2019-04-13 13:30:51 -0400

    Replaced command line inputs with an array of Planet strings.

--- VsopVsSpice/src/vsopvsspice/VsopVsSpice.java
+++ VsopVsSpice/src/vsopvsspice/VsopVsSpice.java
@@ -11,7 +11,13 @@ import java.io.IOException;
 import vsop.DateFuncs;
 import vsop.generated.CoefsGenerated;
 import vsop.generated.EarthCoefsGenerated;
+import vsop.generated.JupiterCoefsGenerated;
+import vsop.generated.MarsCoefsGenerated;
 import vsop.generated.MercuryCoefsGenerated;
+import vsop.generated.NeptuneCoefsGenerated;
+import vsop.generated.SaturnCoefsGenerated;
+import vsop.generated.UranusCoefsGenerated;
+import vsop.generated.VenusCoefsGenerated;
 
 /**
  *
@@ -19,69 +25,86 @@ import vsop.generated.MercuryCoefsGenerated;
  */
 public class VsopVsSpice {
 
+   public static String[] PLANETS_SPICE = new String[]{
+      "MERCURY",
+      "VENUS",
+      "EARTH",
+      "MARS_BARYCENTER",
+      "JUPITER_BARYCENTER",
+      "SATURN_BARYCENTER",
+      "URANUS_BARYCENTER",
+      "NEPTUNE_BARYCENTER"
+   };
+
    /**
     * @param args the command line arguments
     */
    public static void main(String[] args) {
 
-      System.out.println("args.length = " + args.length);
+      for (String Planet : VsopVsSpice.PLANETS_SPICE) {
 
-      String fileName = "D:\\JOrbit\\Spice\\SpiceVsopMain\\" + args[0] + "ECLIPJ2000SUNXyzR.txt";
+         String fileName = "D:\\JOrbit\\Spice\\SpiceVsopMain\\" + Planet
+                 + "-ECLIPJ2000-SUN-XyzR.txt";
 
-      int jdn = DateFuncs.J2000;
-      double etSpice = 0;
-      double xSpice = 0;
-      double ySpice = 0;
-      double zSpice = 0;
-      double rSpice = 0;
-      double tVsop = 0;
-      double etVsop = 0;
-      double xVsop = 0;
-      double yVsop = 0;
-      double zVsop = 0;
-      double rVsop = 0;
-      BufferedReader reader;
-      try {
-         reader = new BufferedReader(new FileReader(fileName));
-         String line = reader.readLine();
-         while (line != null) {
-            //System.out.println(line);
-            String[] tokens = line.split("\\s+");
-            // System.out.println("token[" + i + "] = " + tokens[i]);
-            etSpice = Double.parseDouble(tokens[1]);
-            xSpice = Double.parseDouble(tokens[2]);
-            ySpice = Double.parseDouble(tokens[3]);
-            zSpice = Double.parseDouble(tokens[4]);
-            rSpice = Double.parseDouble(tokens[5]);
-            //
-            // Convert Spice et to VSOP t
-            //
-            tVsop = DateFuncs.t(jdn++);
-            etVsop = DateFuncs.et(tVsop) + DateFuncs.SPICE_T_OFFSET;
-            VsopVsSpice.percentDiff(etVsop, etSpice, "ET");
-            //
-            // TODO: Get planet from filename
-            //
-            CoefsGenerated xyzFuncs = VsopVsSpice.construct(fileName);
+         System.out.println("fileName = " + fileName);
 
-            xVsop = xyzFuncs.X(tVsop) * CoefsGenerated.KM_PER_AU;
-            VsopVsSpice.percentDiff(xVsop, xSpice, "X");
+         int jdn = DateFuncs.J2000;
+         double etSpice = 0;
+         double xSpice = 0;
+         double ySpice = 0;
+         double zSpice = 0;
+         double rSpice = 0;
+         double tVsop = 0;
+         double etVsop = 0;
+         double xVsop = 0;
+         double yVsop = 0;
+         double zVsop = 0;
+         double rVsop = 0;
 
-            yVsop = xyzFuncs.Y(tVsop) * CoefsGenerated.KM_PER_AU;
-            VsopVsSpice.percentDiff(yVsop, ySpice, "Y");
+         BufferedReader reader;
+         try {
+            reader = new BufferedReader(new FileReader(fileName));
+            String line = reader.readLine();
+            while (line != null) {
+               //System.out.println(line);
+               String[] tokens = line.split("\\s+");
+               // System.out.println("token[" + i + "] = " + tokens[i]);
+               etSpice = Double.parseDouble(tokens[1]);
+               xSpice = Double.parseDouble(tokens[2]);
+               ySpice = Double.parseDouble(tokens[3]);
+               zSpice = Double.parseDouble(tokens[4]);
+               rSpice = Double.parseDouble(tokens[5]);
+               //
+               // Convert Spice et to VSOP t
+               //
+               tVsop = DateFuncs.t(jdn++);
+               etVsop = DateFuncs.et(tVsop) + DateFuncs.SPICE_T_OFFSET;
+               VsopVsSpice.percentDiff(etVsop, etSpice, "ET");
+               //
+               // TODO: Get planet from filename
+               //
+               CoefsGenerated xyzFuncs = VsopVsSpice.construct(fileName);
 
-            zVsop = xyzFuncs.Z(tVsop) * CoefsGenerated.KM_PER_AU;
-            VsopVsSpice.percentDiff(zVsop, zSpice, "Z");
+               xVsop = xyzFuncs.X(tVsop) * CoefsGenerated.KM_PER_AU;
+               VsopVsSpice.percentDiff(xVsop, xSpice, "X");
 
-            rVsop = Math.sqrt(xVsop * xVsop + yVsop * yVsop + zVsop * zVsop);
-            VsopVsSpice.percentDiff(rVsop, rSpice, "R");
+               yVsop = xyzFuncs.Y(tVsop) * CoefsGenerated.KM_PER_AU;
+               VsopVsSpice.percentDiff(yVsop, ySpice, "Y");
 
-            System.out.println();
-            line = reader.readLine();
+               zVsop = xyzFuncs.Z(tVsop) * CoefsGenerated.KM_PER_AU;
+               VsopVsSpice.percentDiff(zVsop, zSpice, "Z");
+
+               rVsop = Math.sqrt(xVsop * xVsop + yVsop * yVsop + zVsop * zVsop);
+               VsopVsSpice.percentDiff(rVsop, rSpice, "R");
+
+               System.out.println();
+               line = reader.readLine();
+            }
+            reader.close();
+         } catch (IOException e) {
+            e.printStackTrace();
          }
-         reader.close();
-      } catch (IOException e) {
-         e.printStackTrace();
+
       }
    }
 
@@ -96,12 +119,30 @@ public class VsopVsSpice {
       CoefsGenerated xyzFuncs = null;
       if (fileName.contains("MERCURY")) {
          xyzFuncs = new MercuryCoefsGenerated();
+      } else if (fileName.contains("VENUS")) {
+         xyzFuncs = new VenusCoefsGenerated();
+
       } else if (fileName.contains("EARTH")) {
          xyzFuncs = new EarthCoefsGenerated();
 
+      } else if (fileName.contains("MARS")) {
+         xyzFuncs = new MarsCoefsGenerated();
+
+      } else if (fileName.contains("JUPITER")) {
+         xyzFuncs = new JupiterCoefsGenerated();
+
+      } else if (fileName.contains("SATURN")) {
+         xyzFuncs
+                 = new SaturnCoefsGenerated();
+
+      } else if (fileName.contains("URANUS")) {
+         xyzFuncs = new UranusCoefsGenerated();
+
+      } else if (fileName.contains("NEPTUNE")) {
+         xyzFuncs = new NeptuneCoefsGenerated();
       }
+
       return xyzFuncs;
    }
-   
-}
 
+}
